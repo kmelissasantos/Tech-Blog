8ea99fd8ee3cad88de2d58857b6a0ae4faf61976
Melissa Santos <melissasantos@melissas-mbp.attlocal.net>
2021-07-19 02:56:54 -0400

    added js and handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const {
-    User,
-    Post,
-    Comment
-} = require('../models');
+const { User, Post, Comment } = require('../models');
 
 
 router.get('/', (req, res) => {
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,22 +1,10 @@
-module.exports = {
-    format_date: date => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+function formatDate(date) {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
         date
       ).getFullYear()}`;
-    },
-    format_url: url => {
-      return url
-        .replace('http://', '')
-        .replace('https://', '')
-        .replace('www.', '')
-        .split('/')[0]
-        .split('?')[0];
-    },
-    format_plural: (word, amount) => {
-      if (amount !== 1) {
-        return `${word}s`;
-      }
+}
 
-      return word;
-    }
-};
+
+module.exports = {
+    formatDate
+}
