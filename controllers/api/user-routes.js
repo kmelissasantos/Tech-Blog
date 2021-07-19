800c004c9dd90b71001e50fc150778730dd7889b
Melissa Santos <melissasantos@melissas-mbp.attlocal.net>
2021-07-19 00:09:29 -0400

    updated post and comment models

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -5,6 +5,7 @@ const {
     Post
 } = require('../../models');
 
+//find all
 router.get('/', (req, res) => {
     User.findAll({
             attributes: {
@@ -18,6 +19,45 @@ router.get('/', (req, res) => {
         });
 });
 
+// one user
+router.get('/:id', (req, res) => {
+    User.findOne({
+            attributes: {
+                exclude: ['password']
+            },
+            where: {
+                id: req.params.id
+            },
+            include: [{
+                    model: Post,
+                    attributes: ['id', 'title', 'content', 'created_at']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'created_at'],
+                    include: {
+                        model: Post,
+                        attributes: ['title']
+                    }
+                }
+            ]
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({
+                    message: 'No user found with this id'
+                });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//create
 router.post('/', (req, res) => {
     User.create({
             username: req.body.username,
